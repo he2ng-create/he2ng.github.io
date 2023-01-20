610cc1b41740f64efb1df111a929c6808cd94f6d
heeyeon.kang <iamhe2ng@gmail.com>
2023-01-20 15:39:31 +0900

    2022-01-20 Update:

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "JVM",
+        "excerpt":"0. Overview Purpose JVM의 정의를 알아봅니다. JVM 구조를 알아봅니다. JVM 동작 원리를 알아봅니다. 1. JVM JVM(Java Virtual Machine)은 자바 가상 머신으로 자바 바이트 코드(.class파일)을 OS 특화된 코드로 변환하여 실행 시켜주는 가상의 머신입니다. 바이트 코드를 실행이라고 하면 바이트 코드를 인터프리터와 JIT(Just In Time) 컴파일러를 사용하여 Native OS에 맞추어 머신코드(기계어)로 변형한 다음에...","categories": ["posts","java"],
+        "tags": [],
+        "url": "/posts/java/jvm/",
+        "teaser": null
+      }]
